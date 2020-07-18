690fa39f01f532c29d8be1cadcb6478adf138479
Zooms100 <66204351+Zooms100@users.noreply.github.com>
2020-07-18 02:06:28 -0400

    Add files via upload

---
+++ esx_gridgarage/grid_garage.sql
@@ -0,0 +1,14 @@
+THIS LINE WILL STOP YOUR SCRIPT FROM WORKING. FIND IT IN YOUR .SQL YOU MUST UNDERSTAND THIS IS COPYRIGHT CLAIMED..
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+ALTER TABLE `owned_vehicles` ADD `state` BOOLEAN NOT NULL DEFAULT FALSE AFTER `owner`;
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+--@COPYRIGHT 2020 GRID_GAMING FROM https://forum.cfx.re/u/grid_gaming/summary @COPYRIGHT 2020"
+
+
+
+--Seeing someone upload this script? Contact me here: https://forum.cfx.re/u/grid_gaming/summary
